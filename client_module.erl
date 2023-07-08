5b413165affe3bc8eff39afb4a90e7c252b3d462
William Englund <w.f.englund@gmail.com>
2023-07-08 17:56:15 +0200

    worked on basic communication between client and server

--- client_module.erl
+++ client_module.erl
@@ -1,16 +1,22 @@
 -module(client_module).
 -author("William Englund").
--export([start_client/1, init/1]).
+-behaviour(gen_server).
+-export([start_client/1, init/1, send_message/2]).
 
 start_client(Name) ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [{name, Name}], []).
 
-init([{name, Name}]) ->
-	io:fwrite("~p~n", [Name]),
-	case gen_server:call({global, server_module}, {msg, Name}) of % Send a message to server
+init([{name, Name}]) -> % Make a synchronous call to server to join the chat
+	case gen_server:call({global, server_module}, {memberId, Name}) of % Send a message to server
 		{confirmation, Message} ->
-			io:fwrite("~p~n", [Message]); % Print the message that server sends back
+			io:fwrite("~p has joined the chat~n", [Message]); % Print the message that server sends back
 		{error, Reason} ->
 			io:fwrite("Stopping because of error, reason: ~p~n", [Reason]),
 			{stop, normal}
 	end.
+
+send_message(Message, Name) ->
+	gen_server:cast({global, server_module}, {out_msg, Message, Name}).
+
+handle_cast({in_msg, Message, Name}, _State) ->
+	io:fwrite("~p : ~p~n", [Name, Message]).
--- server_module.erl
+++ server_module.erl
@@ -1,15 +1,41 @@
 -module(server_module).
 -author("William Englund").
 -behaviour(gen_server).
--export([start_server/0, init/1]).
--export([handle_call/3]).
+-export([member_handler/1, start_server/0, init/1]).
+-export([handle_call/3, handle_cast/2]).
+-define(STORAGE, storage).
+
+member_handler(Members) ->
+	receive
+		{addMember, New_member} ->
+			io:fwrite("Member ~p added.~n", [New_member]),
+			member_handler([New_member] ++ Members);
+		{getMembers, PID} ->
+			PID ! {allMembers, Members},
+			member_handler(Members);
+		_ ->
+			member_handler(Members)
+	end.
 
 start_server() ->
+	PID = spawn(?MODULE, member_handler, [[]]),
+	register(?STORAGE, PID),
 	gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
 	{ok, 1}.
 
-handle_call({msg, Message}, _From, State) -> % Receive messages that are sent by call() in client
-	io:fwrite("~p~n", [Message]),
-	{reply, {confirmation, "Message received"}, State}. % Reply back to client
+handle_call({memberId, Name}, From, State) -> % Receive messages that are sent by call() in client
+	io:fwrite("~p~n", [Name]),
+	whereis(?STORAGE) ! {addMember, From},
+	{reply, {confirmation, Name}, State}. % Reply back to client
+
+handle_cast({out_msg, Message, Name}, _State) ->
+	whereis(?STORAGE) ! {getMembers, self()},
+	receive
+		{allMembers, Member_list} ->
+			ok
+	end,
+	io:fwrite("~p~n", [Member_list]),
+	%{noreply, 1}.
+	gen_server:cast(Member_list, {in_msg, Message, Name}). % Does not work yet
