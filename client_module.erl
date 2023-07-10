824bf0e8c9f7d8620dbbabf56d93974b9222abd4
William Englund <w.f.englund@gmail.com>
2023-07-11 00:54:53 +0200

    increase functionality and resolved some errors

--- client_module.erl
+++ client_module.erl
@@ -1,9 +1,22 @@
 -module(client_module).
 -author("William Englund").
 -behaviour(gen_server).
--export([start_client/1, init/1, send_message/2]).
+-export([name_handler/1, start_client/1, init/1, send_message/1]).
+-export([handle_cast/2, handle_call/3]).
+-define(STORAGE, storage).
+
+name_handler(Name) ->
+	receive
+		{getName, PID} ->
+			PID ! {name, Name},
+			name_handler(Name);
+		_ ->
+			name_handler(Name)
+	end.
 
 start_client(Name) ->
+	PID = spawn(?MODULE, name_handler, [Name]),
+	register(?STORAGE, PID),
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [{name, Name}], []).
 
 init([{name, Name}]) -> % Make a synchronous call to server to join the chat
@@ -11,12 +24,21 @@ init([{name, Name}]) -> % Make a synchronous call to server to join the chat
 		{confirmation, Message} ->
 			io:fwrite("~p has joined the chat~n", [Message]); % Print the message that server sends back
 		{error, Reason} ->
-			io:fwrite("Stopping because of error, reason: ~p~n", [Reason]),
-			{stop, normal}
-	end.
+			io:fwrite("Stopping because of error, reason: ~p~n", [Reason])
+	end,
+	{ok, 1}.
 
-send_message(Message, Name) ->
+send_message(Message) ->
+	whereis(?STORAGE) ! {getName, self()},
+	receive
+		{name, Name} ->
+			ok
+	end,
 	gen_server:cast({global, server_module}, {out_msg, Message, Name}).
 
 handle_cast({in_msg, Message, Name}, _State) ->
-	io:fwrite("~p : ~p~n", [Name, Message]).
+	io:fwrite("~p : ~p~n", [Name, Message]),
+	{noreply, 1}.
+
+handle_call(_, _, _) ->
+	ok.
